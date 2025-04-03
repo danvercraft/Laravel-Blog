a3f76654d3a4458332d2d27b6bb71ee9e9747565
Danvercraft <san290200@gmail.com>
2025-04-02 19:37:25 -0500

    Optimizacion UI Categorias 1.2

--- resources/views/finance_categories/index.blade.php
+++ resources/views/finance_categories/index.blade.php
@@ -13,9 +13,9 @@
                 </div>
             </div>
         </div>
-        <div class="relative h-full flex-1 overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
-            <div class="overflow-x-auto h-full bg-neutral-900 text-neutral-100">
-                <table class="min-w-full divide-y divide-neutral-700">
+        <div class="relative  h-full flex-1 overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
+            <div class=" mx-auto overflow-x-auto h-full bg-neutral-900 text-neutral-100">
+                <table class="mx-auto min-w-full divide-y divide-neutral-700 p-2">
                     <thead class="bg-neutral-800">
                         <tr>
                             <th scope="col" class="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-neutral-300">
@@ -40,16 +40,14 @@
                                 </td>
                                 <td class="px-6 py-4 text-sm">
                                     <div class="flex space-x-2">
-                                        <a href="{{-- route('finance_categories.edit', $category) --}}" class="px-4 py-2 text-sm font-medium text-white bg-blue-600 rounded-lg hover:bg-blue-700">
+                                        <flux:button variant="primary"><flux:icon.pencil-square variant="mini" /></flux:button>
+                                        <flux:button variant="danger"><flux:icon.trash variant="mini" /></flux:button>
+
+                                        {{-- <a href="{{-- route('finance_categories.edit', $category) " class="px-4 py-2 text-sm font-medium text-white bg-amber-300 rounded-lg hover:bg-amber-300">
                                             Editar
                                         </a>
-                                        <form action="{{-- route('finance_categories.destroy', $category) --}}" method="POST" onsubmit="return confirm('¿Estás seguro de que deseas eliminar esta categoría?');">
-                                            @csrf
-                                            @method('DELETE')
-                                            <button type="submit" class="px-4 py-2 text-sm font-medium text-white bg-red-600 rounded-lg hover:bg-red-700">
-                                                Eliminar
-                                            </button>
-                                        </form>
+                                        --}}
+
                                     </div>
                                 </td>
                             </tr>
